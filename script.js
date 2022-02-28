93e9be46f5126e11933435ece97105dcec6474e6
potatonaaa <100538231+potatonaaa@users.noreply.github.com>
2022-02-28 12:30:52 +0700

    init commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+const links = document.querySelectorAll(".navbar-item");
+links.forEach((item) => {
+    item.addEventListener("click", () => {
+        const el = document.getElementById(item.getAttribute("data-link"));
+        el.scrollIntoView({ behavior: "smooth", block: "center" })
+    });
+});
